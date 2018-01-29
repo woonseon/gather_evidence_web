1a7562ad9cb40f167c9ee7d31ac6082ecf5f96b5
woonseon <dbdnstjs8072@naver.com>
2018-01-30 02:16:52 +0900

    second demo:

--- g_evidence.py
+++ g_evidence.py
@@ -1,5 +1,9 @@
 from PyQt4 import QtCore, QtGui, QtWebKit
 import sys
+import datetime
+import piexif
+
+today = str(datetime.datetime.now())
 
 class Browser(QtGui.QMainWindow):
     def __init__(self):
@@ -42,6 +46,7 @@ class Browser(QtGui.QMainWindow):
 
         self.html = QtWebKit.QWebView()
         self.html.loadFinished.connect(self.get_html)
+        #self.html.loadFinished.connect(self.render)
         self.gridLayout.addWidget(self.html)
         self.mainLayout.addWidget(self.frame)
         self.setCentralWidget(self.centralwidget)
@@ -62,36 +67,59 @@ class Browser(QtGui.QMainWindow):
 
     def browse(self):
         url = self.tb_url.text() if self.tb_url.text() else self.default_url
+        self.url_name = url.split('//')[1]
         self.html.load(QtCore.QUrl(url))
         self.html.show()
 
     def get_total_window(self):
-        QtGui.QMessageBox.about(self, "Warning", "This is not csv file")
+        try:
+            self.html.page().setViewportSize(self.html.page().mainFrame().contentsSize())
+            image = QtGui.QImage(self.html.page().viewportSize(), QtGui.QImage.Format_ARGB32)
+
+            painter = QtGui.QPainter()
+            painter.begin(image)
+            self.html.page().mainFrame().render(painter)
+            painter.end()
+
+            file_name = self.url_name + '_' + today + '_total.jpg'
+            print file_name
+            image.save(file_name)
+            
+            QtGui.QMessageBox.about(self, "Success", "Save Full Screen!!")
+        except:
+            QtGui.QMessageBox.about(self, "Warning", "Can't save full screen!!")
     
     def get_current_window(self):
-        # self.html.loadFinished.connect(self.render)
-        self.render
+        try:
+            self.html.page().mainFrame().contentsSize()
+            image = QtGui.QImage(self.html.page().viewportSize(), QtGui.QImage.Format_ARGB32)
+
+            painter = QtGui.QPainter()
+            painter.begin(image)
+            self.html.page().mainFrame().render(painter)
+            painter.end()
+
+            file_name = self.url_name + '_' + today + '_current.jpg'
+            image.save(file_name)
+            
+            QtGui.QMessageBox.about(self, "Success", "Save Current Screen!!")
+        except:
+            QtGui.QMessageBox.about(self, "Warning", "Can't save current screen!!")
     
     # get html src
     def get_src(self):
-        page = self.html.page()
-        frame = page.mainFrame()
-        html_code = unicode(frame.toHtml())
-        print html_code
-    
-    def render(self):
-        self.html.setViewportSize(self.html.mainFrame().contentsSize())
-        image = QtGui.QImage(self.html.viewportSize(), QtGui.QImage.Format_ARGB32)
-        print image
-
-        painter = QtGui.QPainter()
-        painter.begin(image)
-        self.webPage.mainFrame().render(painter)
-        painter.end()
-
-        image.save('./thumbnail.png')
-        QApplication.exit()
-        
+        try:
+            page = self.html.page()
+            frame = page.mainFrame()
+            html_code = unicode(frame.toHtml())
+            
+            file_name = self.url_name + '_' + today + '_src.txt'
+            f = open(file_name, 'w')
+            f.write(html_code.encode("utf-8"))
+            f.close()
+            QtGui.QMessageBox.about(self, "Success", "Save html src!!")
+        except:
+            QtGui.QMessageBox.about(self, "Warning", "Can't save html src!!")
 
 if __name__ == '__main__':
     qapp = QtGui.QApplication(sys.argv)
